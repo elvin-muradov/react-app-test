b637160e0b4612a973c103c01871a24ab0d65c57
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-01-11 22:57:39 +0400

    test react project codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo2.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} alt="Logo"/>
+                <p>
+                    Cyka Blyat
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
